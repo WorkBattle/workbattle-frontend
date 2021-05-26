fb671c763830d8cd935436c08c46546be272a0c9
Arthur <arthionweb@gmail.com>
2021-05-27 00:08:27 +0300

    JWT Interceptor add Credentials

--- src/lib/interceptor/jwt.interceptor.ts
+++ src/lib/interceptor/jwt.interceptor.ts
@@ -1,18 +1,26 @@
-import {Injectable} from '@angular/core';
-import {HttpEvent, HttpInterceptor, HttpHandler, HttpRequest} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import { Injectable } from '@angular/core';
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-
   constructor() {}
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
     if (localStorage.getItem('token')) {
       req = req.clone({
+        withCredentials: true,
         setHeaders: {
-          Authorization: `Bearer ${localStorage.getItem('token')}`
-        }
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
       });
     }
     return next.handle(req);
